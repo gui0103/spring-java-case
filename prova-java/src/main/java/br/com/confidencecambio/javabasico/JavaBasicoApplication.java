40f3f6d56c5920b233d9a98e81068977f843a8cb
gui0103 <gui03costa@gmail.com>
2022-11-02 04:00:23 -0300

    Name Methods upgrade

--- prova-java/src/main/java/br/com/confidencecambio/javabasico/JavaBasicoApplication.java
+++ prova-java/src/main/java/br/com/confidencecambio/javabasico/JavaBasicoApplication.java
@@ -9,7 +9,9 @@ public class JavaBasicoApplication {
 
 	public static void main(String[] args) {
 
-		String teste = "     Guilherme Carvalho Costa ";
+		String teste = "     Guilherme de Carvalho Costa ";
+
+		String teste2 = "     Guilherme Carvalho Costa ";
 
 		Cliente cliente = new Cliente(teste);
 
@@ -20,6 +22,17 @@ public class JavaBasicoApplication {
 		System.out.println("Sobrenome(s) do Cliente: " + cliente.getLastNames());
 		System.out.println("Nome completo do Cliente em letra maiúscula: " + cliente.getUpperCasedName());
 		System.out.println("Nome completo do Cliente abreviado: " + cliente.getShortName());
+		System.out.println("\n \n \n \n \n");
+
+		Cliente cliente2 = new Cliente(teste2);
+
+		cliente2.setName(teste2);
+
+		System.out.println("Nome completo do Cliente: " + cliente2.getName());
+		System.out.println("Primeiro nome do Cliente: " + cliente2.getFirstName());
+		System.out.println("Sobrenome(s) do Cliente: " + cliente2.getLastNames());
+		System.out.println("Nome completo do Cliente em letra maiúscula: " + cliente2.getUpperCasedName());
+		System.out.println("Nome completo do Cliente abreviado: " + cliente2.getShortName());
 
 		SpringApplication.run(JavaBasicoApplication.class, args);
 	}
--- prova-java/src/main/java/br/com/confidencecambio/javabasico/entity/User.java
+++ prova-java/src/main/java/br/com/confidencecambio/javabasico/entity/User.java
@@ -2,6 +2,7 @@ package br.com.confidencecambio.javabasico.entity;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.stream.Stream;
@@ -35,12 +36,38 @@ public abstract class User {
     public String getShortName() {
         String[] names = this.name.split(" ");
         char letter = names[1].charAt(0);
-        String shortName = String.format("%s %c.", names[0], letter);
+        String response = "";
 
-        StringBuffer lastNames = new StringBuffer(getLastNames());
-        int index = lastNames.indexOf(" ");
+        // Validação para casos específicos como o meu nome:
+        // "Guilherme de Carvalho Costa"
+        // A abreviação correta seria "Guilherme C. Costa", ignorando o "de"
+        if (Character.isLowerCase(letter)) {
+            char letterUpperCase = names[2].charAt(0);
+            String shortName = String.format("%s %s %c.", names[0], names[1], letterUpperCase);
 
-        return shortName + " " + lastNames.delete(0, index + 1);
+            // de Carvalho Costa
+            response = shortName + removeWords();
+        }
+        else {
+            String shortName = String.format("%s %c.", names[0], letter);
+
+            StringBuffer lastNames = new StringBuffer(getLastNames());
+            int index = lastNames.indexOf(" ");
+
+            return shortName + " " + lastNames.delete(0, index + 1);
+        }
+        return response;
+    }
+
+    public String removeWords() {
+        String[] array = getLastNames().split(" ");
+        StringBuilder response = new StringBuilder(String.format(""));
+            for (int i = 0; i < array.length; i++) {
+                if (i > 1) {
+                    response.append(" ").append(array[i].toString());
+                }
+            }
+            return response.toString();
     }
 
     public String getName() {
